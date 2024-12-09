1716058779bf9910fa10c7d60e4ac2dfc951f879
Islem-Maaiti <islem.maiti@esprit.tn>
2024-12-10 00:51:45 +0100

    7

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -6,12 +6,14 @@ import Education from "../pages/education/EducationComponent";
 import Experience from "../pages/experience/Experience";
 import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
+//import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
+import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
   render() {
     return (
-      <BrowserRouter basename="/amira-khalfi-website">
+      <BrowserRouter basename="/amira-khalfi-website"> {/* Corrected basename */}
         <Switch>
           <Route
             path="/"
@@ -25,40 +27,48 @@ export default class Main extends Component {
             }
           />
           <Route
-            path="/amira-khalfi-website/home"
+            path="/amira-khalfi-website/home" // Updated path
             render={(props) => <Home {...props} theme={this.props.theme} />}
           />
           <Route
-            path="/amira-khalfi-website/education"
-            render={(props) => (
-              <Education {...props} theme={this.props.theme} />
-            )}
-          />
-          <Route
-            path="/amira-khalfi-website/experience"
+            path="/amira-khalfi-website/experience" // Updated path
             exact
             render={(props) => (
               <Experience {...props} theme={this.props.theme} />
             )}
           />
-          
           <Route
-            path="/amira-khalfi-website/opensource"
+            path="/amira-khalfi-website/education" // Updated path
+            render={(props) => (
+              <Education {...props} theme={this.props.theme} />
+            )}
+          />
+          <Route
+            path="/amira-khalfi-website/contact" // Updated path
+            render={(props) => <Contact {...props} theme={this.props.theme} />}
+          />
+          <Route
+            path="/amira-khalfi-website/opensource" // Updated path
             render={(props) => (
               <Opensource {...props} theme={this.props.theme} />
             )}
           />
-          <Route
-            path="/amira-khalfi-website/contact"
-            render={(props) => <Contact {...props} theme={this.props.theme} />}
-          />
 
           {/* {settings.isSplash && (
             <Route
-              path="/amira-khalfi-website/splash"
+              path="/amira-khalfi-website/splash" // Updated path
               render={(props) => <Splash {...props} theme={this.props.theme} />}
             />
           )} */}
+
+          {/* <Route
+            path="/Islem-Maiti_Portfolio/projects" // Updated path
+            render={(props) => <Projects {...props} theme={this.props.theme} />}
+          /> */}
+          <Route
+            path="*"
+            render={(props) => <Error404 {...props} theme={this.props.theme} />}
+          />
         </Switch>
       </BrowserRouter>
     );
